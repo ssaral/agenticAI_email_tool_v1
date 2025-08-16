0d253ddfa80c4c8412011158f609916807659be5
ssaral <ssaral.india@gmail.com>
2025-08-16 16:51:03 +0530

    Added main python file for backend check

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 # main.py
 
 from agent.gmail import fetch_emails
+from agent.llm_agent import process_emails
 
 if __name__ == "__main__":
-    emails = fetch_emails(n=5)
-    for e in emails:
-        print("-" * 50)
-        print("From:", e["from"])
-        print("Subject:", e["subject"])
-        print("Body:", e["body"])
+    emails = fetch_emails(n=1)
+    # for e in emails:
+    #     print("-" * 50)
+    #     print("From:", e["from"])
+    #     print("Subject:", e["subject"])
+    #     print("Body:", e["body"])
+    
+    process_emails(emails)
+
